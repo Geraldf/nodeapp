9bfacafabceb5558b7e23fe9f242e3375f67ef65
Gerald Fuchs <gerald@fuchsclan.de>
2018-02-09 16:10:37 +0100

    worked on Process

--- dockerfile
+++ dockerfile
@@ -1,9 +1,20 @@
-FROM readytalk/nodejs
+FROM node:7.7.2-alpine
 
-WORKDIR /app
-ADD package.json /app/
-RUN npm install
-ADD . /app
+ENV http_proxy http://10.0.128.11:3128
+ENV https_proxy https://10.0.128.11:3128
 
-CMD []
-ENTRYPOINT ["/nodejs/bin/npm", "start"]
+
+WORKDIR /usr/app
+
+
+RUN echo --------
+RUN echo http_proxy
+
+RUN env
+
+COPY package.json .
+
+RUN npm config set https-proxy http://10.0.128.11:3128 \
+    && npm install 
+
+COPY . .
--- routes/process.js
+++ routes/process.js
@@ -2,45 +2,43 @@ var express = require('express');
 var router = express.Router();
 var https = require('https');
 var c_json = require('../config/default.json')
+var request = require('request-promise');
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  getCamunda();
+  getCamunda(res);
   res.render('process', {
     title: 'process'
   });
 });
 
-function getCamunda() {
-  var optionsget = {
-    host: c_json.camunda.host,
-    // host : 'graph.facebook.com', // here only the domain name
-    // (no http/https !)
-    port: 8080,
-    path: 'engine-rest/process-definition?latest=true', // the rest of the url with parameters if needed
-    method: 'GET' // do GET
+function getCamunda(res) {
+  var options = {
+    uri: c_json.camunda.host + ":8080/engine-rest/process-definition?latest=true",
+    // qs: {
+    //     access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx'
+    // },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
   };
-  //http://192.168.99.100:8080/engine-rest/process-definition?latest=true
-  var reqGet = https.request(optionsget, function (res) {
-    console.log("statusCode: ", res.statusCode);
-    // uncomment it for header details
-    //  console.log("headers: ", res.headers);
+  request(options)
+    .then(function (repos) {
+      showProcesses(repos,res);
+    })
+    .catch(function (err) {
+      console.log("ERROR %s", err);
+      // API call failed...    
+    })
+}
+//http://192.168.99.100:8080/engine-rest/process-definition?latest=tru
 
 
-    res.on('data', function (d) {
-      console.info('GET result:\n');
-      process.stdout.write(d);
-      console.info('\n\nCall completed');
-    });
-
-  });
-
-  reqGet.end();
-  reqGet.on('error', function (e) {
-    console.error(e);
-  });
-
+function showProcesses(processList,res){
+  //console.log(JSON.stringify(processList));
+  res.render('process', { pList : processList });
 }
 
 
